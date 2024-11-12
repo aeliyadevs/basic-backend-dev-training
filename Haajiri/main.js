2495859d3e676b7584402ff9efee6eb5aceab081
aeliyadevs <aeliyadevs@gmail.com>
2024-11-13 00:05:14 +0545

    Haajirii - firebase firestore setup

---
+++ Haajiri/main.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+import firebaseConfig from "./config";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log(db);
