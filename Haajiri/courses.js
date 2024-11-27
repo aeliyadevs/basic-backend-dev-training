17ddc8d3d8acf1c367fc9dad01305e7d67633b02
aeliyadevs <aeliyadevs@gmail.com>
2024-11-27 11:42:05 +0545

    Haajirii - create and append relavant elements for course cards to display them on page with styles

--- Haajiri/courses.js
+++ Haajiri/courses.js
@@ -4,7 +4,6 @@ import {
   getFirestore,
   collection,
   getDocs,
-  addDoc,
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 // Import forebase configuration details
@@ -20,18 +19,33 @@ const courses = await getDocs(collection(db, "courses"));
 const coursesDiv = document.getElementById("courses");
 courses.forEach((course) => {
   console.log(course.data());
-  const card = document.createElement("div");
-  card.className = "course-card";
-  card.innerHTML = `
-    <h3>${course.data().courseTitle}</h3>
-    <div class="status running">Running</div>
-    <p>${course.data().courseDesc}</p>
-    <p><strong>Start Date: ${course.data().startDate}</strong></p>
-    <p><strong>Instructor: ${course.data().instructor}</strong></p>
-    <div class="options">
-      <a href="students.html" class="primary-btn">Manage Students</a>
-      <a href="#" class="primary-btn">Take Attendance</a>
-    </div>
-  `;
-  coursesDiv.appendChild(card);
+  const courseDiv = document.createElement("div");
+  courseDiv.className = "course-card";
+
+  const h3 = document.createElement("h3");
+  h3.innerHTML = course.data().courseTitle;
+
+  const desc = document.createElement("p");
+  desc.innerHTML = course.data().courseDesc;
+
+  courseDiv.appendChild(h3);
+  courseDiv.appendChild(desc);
+
+  document.getElementById("courses").appendChild(courseDiv);
 });
+// courses.forEach((course) => {
+//   const card = document.createElement("div");
+//   card.className = "course-card";
+//   card.innerHTML = `
+//     <h3>${course.data().courseTitle}</h3>
+//     <div class="status running">Running</div>
+//     <p>${course.data().courseDesc}</p>
+//     <p><strong>Start Date: ${course.data().startDate}</strong></p>
+//     <p><strong>Instructor: ${course.data().instructor}</strong></p>
+//     <div class="options">
+//       <a href="students.html" class="primary-btn">Manage Students</a>
+//       <a href="#" class="primary-btn">Take Attendance</a>
+//     </div>
+//   `;
+//   coursesDiv.appendChild(card);
+// });
